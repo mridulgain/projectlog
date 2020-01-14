67a457021eeca88b33615b7ce08a26fb291ef41d
mridulgain <mridulgain@gmail.com>
2020-01-14 05:57:59 +0530

    new feature under construction

---
+++ meetinglist.php
@@ -0,0 +1,7 @@
+<h4> meeting log </h4>
+
+	<textarea name="meeting_minutes" cols = '70' rows = "20">Write meeting minutes here</textarea><br>
+	<input type = 'date' name = 'appointment'><br>
+	<input type = "submit" name = "submit" value="Add">
+
+<font size="3" color="red">Under construction!</font>
